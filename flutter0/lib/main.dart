5cb53719f3e74cfa72c3ce934d50c1820ed598a2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-16 11:31:14 +0700

    1234

--- flutter0/lib/main.dart
+++ flutter0/lib/main.dart
@@ -1,36 +1,3 @@
-import 'package:flutter/material.dart';
+void main(){
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "CMApp",
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("CMapp"),
-      ),
-      body: Container(color: Colors.amberAccent,),
-    );
-  }
 }
