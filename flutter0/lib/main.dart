ebce1f24ae473deb59804988b3f57c47e2245925
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-05-16 11:32:47 +0700

    1234

--- flutter0/lib/main.dart
+++ flutter0/lib/main.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
+
 void main(){
 
+}
+
+class AZApp extends StatelessWidget {
+  const AZApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
